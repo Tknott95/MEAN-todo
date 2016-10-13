0d5446421edff471e4f5dbd198325afc3e6d5009
tknott95 <tknott95@hotmail.com>
2016-10-12 22:59:23 -0600

    MongoDb Post requests complete

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://tknott95:sexytexy@ds023105.mlab.com:23105/mean-todos-db',['todos']);
 
+
+
+// Get Todos
 router.get('/todos', function(req, res, next){
 	db.todos.find(function(err, todos){
 		if(err){
@@ -12,5 +15,23 @@ router.get('/todos', function(req, res, next){
 		}
 	});
 });
+
+// Get Single Todo
+router.get('/todo/:id', function(req, res, next){
+	db.todos.findOne({
+		_id: mongojs.ObjectId(req.params.id)
+	}, function(err, todo){
+		if(err){
+			res.send(err);
+		} else 	{
+			res.json(todo);
+		}
+	});
+});
+
+
+//Save Todo
+
+
 
 module.exports = router;
