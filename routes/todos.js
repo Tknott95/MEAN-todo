e01eda4639723282db84d13d0366091e724860d8
tknott95 <tknott95@hotmail.com>
2016-10-12 22:47:23 -0600

    Database setup through mLab - Mongodb

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://tknott95:sexytexy@ds023105.mlab.com:23105/mean-todos-db',['todos']);
 
 router.get('/todos', function(req, res, next){
-	res.send('TODOS API');
+	db.todos.find(function(err, todos){
+		if(err){
+			res.send(err);
+		} else 	{
+			res.json(todos);
+		}
+	});
 });
 
 module.exports = router;
