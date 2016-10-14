bc221210de5180b51d8fe9a42bc087b4ad9cd020
tknott95 <tknott95@hotmail.com>
2016-10-14 13:42:45 -0600

    HTML , TSC, CSS set for todo

---
+++ client/app/components/todos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-todos',
+    templateUrl: 'todos.component.html'
+})
+export class TodosComponent {
+    constructor() { }
+
+   
+}
