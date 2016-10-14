050ec2952df9f37caac34acc328cfddf2fa89001
tknott95 <tknott95@hotmail.com>
2016-10-14 14:18:00 -0600

    Add todo API call working

--- client/app/services/todo.service.ts
+++ client/app/services/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 
@@ -13,6 +13,16 @@ export class TodoService{
         return this._http.get('/api/v1/todos')
             .map(res => res.json());
     }
+
+    saveTodo(todo){
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this._http.post('/api/v1/todo', JSON.stringify(todo), {headers: headers})
+            .map(res => res.json());
+
+    }
+
+
 }
 
 
